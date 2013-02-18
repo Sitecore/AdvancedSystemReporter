49a79cfd5191a11c2d84a631d71d5ea7bd9a9baa
Raul Jimenez <rj@sitecore.net>
2013-02-18 16:07:44 +0000

    fixed OwnedItemsScanner so it displays items with any owner when owner not set

--- Reports/Scanners/OwnedItemsScanner.cs
+++ Reports/Scanners/OwnedItemsScanner.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using ASR.Interface;
-using ASR.Reports.Items.Exceptions;
-using Sitecore.Data;
-using Sitecore.Data.Items;
 
 namespace ASR.Reports.Scanners
 {
     class OwnedItemsScanner : DatabaseScanner
     {
-        
+
         public string User { get; set; }
-      
+
         public override ICollection Scan()
         {
+            if (string.IsNullOrEmpty(User))
+            {
+                return GetRootItem().Axes.GetDescendants().Where(
+                i => !string.IsNullOrEmpty(i.Security.GetOwner())).ToList();
+            }
+
             return GetRootItem().Axes.GetDescendants().Where(
                 i => i.Security.GetOwner().Equals(User, StringComparison.CurrentCultureIgnoreCase)).ToList();
         }
